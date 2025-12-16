1158869829d2f50663bbc8fdfda816a2cd9019bb
Murilo-Caxambu <mcaxambu.monteiro@gmail.com>
2025-12-16 11:41:49 -0300

    Refatorei o código do tabuleiro de xadrez para usar numpy e simplifiquei a criação do tabuleiro 8x8

--- 01_numpy_basics/chess_grid.py
+++ 01_numpy_basics/chess_grid.py
@@ -1,13 +1,6 @@
 # Tabuleiro de xadrez 8x8 -> vou ter q intercalar entre brancas e pretas: 0 1 0 1 0 1 0 1 dps inverte, 8 colunas
 import numpy as np
-tabuleiro = np.array([0,1,0,1,0,1,0,1, 
-                      1,0,1,0,1,0,1,0,
-                      0,1,0,1,0,1,0,1, 
-                      1,0,1,0,1,0,1,0,
-                      0,1,0,1,0,1,0,1, 
-                      1,0,1,0,1,0,1,0,
-                      0,1,0,1,0,1,0,1, 
-                      1,0,1,0,1,0,1,0,])
-
-n_tabuleiro = tabuleiro.reshape(8,8)
-print (n_tabuleiro)
+tabuleiro = np.zeros((8,8), dtype="int")
+tabuleiro[0::2,1::2] = 1
+tabuleiro [1::2,0::2] = 1
+print(tabuleiro)
