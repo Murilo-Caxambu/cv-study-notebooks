df87593f50ef69fedd6d15557e52a02f368dcd39
Murilo-Caxambu <mcaxambu.monteiro@gmail.com>
2025-12-16 11:52:37 -0300

    Refatorei a criação do tabuleiro de xadrez em uma função para melhor organização do código

--- 01_numpy_basics/chess_grid.py
+++ 01_numpy_basics/chess_grid.py
@@ -1,6 +1,9 @@
 # Tabuleiro de xadrez 8x8 -> vou ter q intercalar entre brancas e pretas: 0 1 0 1 0 1 0 1 dps inverte, 8 colunas
 import numpy as np
-tabuleiro = np.zeros((8,8), dtype="int")
-tabuleiro[0::2,1::2] = 1
-tabuleiro [1::2,0::2] = 1
-print(tabuleiro)
+def criar_grid_tabuleiro():
+    tabuleiro = np.zeros((8,8), dtype="int")
+    tabuleiro[0::2,1::2] = 1
+    tabuleiro [1::2,0::2] = 1
+    return tabuleiro
+tabuleiro_criado = criar_grid_tabuleiro()
+print(tabuleiro_criado)
